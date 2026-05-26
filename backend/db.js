579f36c3f40f664a9fce7589be096b92c17d4b12
syroa3336-cmd <syroa3336@gmail.com>
2026-05-26 14:07:27 +0000

    Deploy frontend to GitHub Pages

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-export const openDb = async () => {
-  return open({
-    filename: './database.sqlite',
-    driver: sqlite3.Database
-  });
-};
